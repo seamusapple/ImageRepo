7920a0e294d76982530a20b17b3dda5956de177e
Dong Pan <dong.pan@essexlg.com>
2022-09-08 16:35:04 +0800

    Finish contact page.

--- LastDemo/scripts/global.js
+++ LastDemo/scripts/global.js
@@ -348,7 +348,10 @@ function prepareForms() {
     var thisform = document.forms[i];
     resetFields(thisform);
     thisform.onsubmit = function () {
-      return validateForm(this);
+      if (!validateForm(this)) return false;
+      var article = document.getElementsByTagName("article")[0];
+      if (submitFormWithAjax(this, article)) return false;
+      return true;
     };
   }
 }
@@ -379,6 +382,68 @@ function validateForm(whichform) {
     }
     return true;
   }
+}
+
+function getHTTPObject() {
+  if (typeof XMLHttpRequest !== "undefined")
+    XMLHttpRequest = function () {
+      try {
+        return new ActiveXObject("Msxml2.XMLHTTP.6.0");
+      } catch (e) {}
+      try {
+        return new ActiveXObject("Msxml2.XMLHTTP.3.0");
+      } catch (e) {}
+      try {
+        return new ActiveXObject("Msxml2.XMLHTTP");
+      } catch (e) {}
+      return false;
+    };
+  return new XMLHttpRequest();
+}
+
+function displayAjaxLoading(element) {
+  while (element.hasChildNodes()) {
+    element.removeChild(element.lastChild);
+  }
+  var content = document.createElement("img");
+  content.setAttribute("src", "images/loading.gif");
+  content.setAttribute("alt", "Loading...");
+  element.appendChild(content);
+}
+
+function submitFormWithAjax(whichform, thetarget) {
+  var request = getHTTPObject();
+  if (!request) {
+    return false;
+  }
+  displayAjaxLoading(thetarget);
+  var dataParts = [];
+  var element;
+  for (var i = 0; i < whichform.elements.length; i++) {
+    element = whichform.elements[i];
+    dataParts[i] = element.name + "=" + encodeURIComponent(element.value);
+  }
+  var data = dataParts.join("&");
+  request.open("POST", whichform.getAttribute("action"), true);
+  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  request.onreadystatechange = function () {
+    if (request.readyState === 4) {
+      if (request.status === 200 || request.status === 0) {
+        var matches = request.responseText.match(
+          /<article>([\s\S]+)<\/article>/
+        );
+        if (matches.length > 0) {
+          thetarget.innerHTML = matches[1];
+        } else {
+          thetarget.innerHTML = "<p>Oops, there was an error. Sorry. </p>";
+        }
+      } else {
+        thetarget.innerHTML = "<p>" + request.statusText + "</p>";
+      }
+    }
+  };
+  request.send(data);
+  return true;
 }
 
 addLoadEvent(highlightPage);
